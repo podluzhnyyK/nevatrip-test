9d4a83e24c779dd6144511add7e69474c93f775e
podluzhnyyK <podluzhnyy94@mail.ru>
2021-05-16 11:02:48 +0500

    2 задание и скрипт

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,10 +2,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 
-const btns = document.getElementsByClassName('offer-info__time-item--more');
-const par = document.getElementsByClassName('offer-info__time');
-btns.onclick = function() {
-    par.classList.add('more-active');
-    console.log('4');
-};
+const list = document.querySelectorAll('.offer-info__time');
+const li = document.createElement('li');
+const span = document.createElement('span');
 
+li.className = 'offer-info__time-item offer-info__time-item--more';
+span.className = 'offer-info__time-title';
+span.innerHTML = 'eщё...';
+li.append(span);
+
+for (const elem of list) {
+    if (elem.children.length > 4) {
+        elem.insertBefore(li, elem.children[3]);
+    }
+}
+
+document.querySelector('.offer-info__time-item--more').onclick = function() {
+    document.querySelector('.offer-info__time-item--more')
+        .parentNode.classList.toggle('more-active');
+};
