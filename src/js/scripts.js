5acedde407ec3b902f751fa03313a2b52d1f1e9e
podluzhnyyK <podluzhnyy94@mail.ru>
2021-05-26 16:43:55 +0500

    Поправил замечания

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,21 +3,20 @@
 /* eslint-disable no-undef */
 
 const list = document.querySelectorAll('.offer-info__time');
-const li = document.createElement('li');
 const span = document.createElement('span');
 
-li.className = 'offer-info__time-item offer-info__time-item--more';
-span.className = 'offer-info__time-title';
+span.className = 'offer-info__time-title offer-info__time-item--more';
 span.innerHTML = 'eщё...';
-li.append(span);
 
 for (const elem of list) {
     if (elem.children.length > 4) {
-        elem.insertBefore(li, elem.children[3]);
+        elem.insertBefore(span, elem.children[3]);
     }
 }
 
 document.querySelector('.offer-info__time-item--more').onclick = function() {
     document.querySelector('.offer-info__time-item--more')
-        .parentNode.classList.toggle('more-active');
+        .parentNode.classList.add('more-active');
+    const node = document.querySelector('.offer-info__time-item--more');
+    node.parentNode.removeChild(node);
 };
